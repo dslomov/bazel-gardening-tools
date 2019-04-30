b0a446c10e0dbed9338e85321df4f6f2d2d3d1b5
Jin <jin@users.noreply.github.com>
2019-04-30 10:50:19 -0400

    ignore releases for gardening (#32)

--- reports.py
+++ reports.py
@@ -387,7 +387,8 @@ def issues_to_garden(reporter, issues, stale_for_days):
             and not issue["assignee"] \
             and not is_pull_request(issue) \
             and is_stale(issue, stale_for_days) \
-            and not work_in_progress(issue)
+            and not work_in_progress(issue) \
+            and not has_label(issue, "release")
 
     reporter(
         issues,
